f35ffbd14bf9b1af59101978b4b1e0baaf38370b
Paiva <danpaiva@live.com>
2024-07-29 14:30:08 -0300

    feat: add code

---
+++ CadastroPessoas/CadastroPessoas/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using CadastroPessoas.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CadastroPessoas.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+
+    }
+    public DbSet<Pessoa> Pessoas { get; set; } = null!;
+}
